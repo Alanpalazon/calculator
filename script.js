f7141302d8572763105d4059e0ba1878396be0a7
Alan <alanpalazon@gmail.com>
2021-07-10 21:44:50 +0100

    Project finished. Future aim to refactor and add in comma number separation and to display full history of calculations until clear button pressed.

--- script.js
+++ script.js
@@ -4,38 +4,35 @@ let calcs = [];
 const display1 = document.getElementById('display-child-1');
 const display2 = document.getElementById('display-child-2');
 display2.textContent = "0";
-//all maths functions need to round long decimal number: find out how to do this. 
+
+
 function modulus(a,b){ return a%b;} 
 function add(a,b){ return a+b;}
 function subtract(a,b){ return a-b;}
 function multiply(a,b){ return a*b;} 
 function divide(a,b){ 
     if(b == 0) {
-        window.alert("Cannot divide by 0"); return 0;
+        window.alert("Cannot divide by 0"); 
+        return 0;
     }else{
         return a/b;
     }
-}
+}   
 
 function numberClick(e){
     lastClicked = e.target.classList;
-
-    if(display2.textContent != calcs[calcs.length-2]){
+     if(display2.textContent != calcs[calcs.length-2]){
         if(display2.textContent.length == 1 && display2.textContent == "0"){
             display2.textContent = e.target.textContent;
-        
             return;
         }else{
             display2.textContent += e.target.textContent;
-        
             return;
         }
     }else{
         display2.textContent = e.target.textContent;
-    
         return;
     }   
-
 
 }
 
@@ -43,15 +40,12 @@ function decimalClick(e){
     lastClicked = e.target.classList;
     if(display2.textContent == calcs[calcs.length-2]){
         display2.textContent = "0.";
-    
     }else{
           if(!display2.textContent.includes(".")){
                 if(display2.textContent.length == 0){
-                    display2.textContent = "0.";
-                
+                    display2.textContent = "0."; 
             }else{
                     display2.textContent += e.target.textContent
-                
                 }
             }
     }
@@ -151,30 +145,35 @@ function operatorClick(e){
 
 function equals(e){
     if(calcs.length >=2){
-        if(calcs[calcs.length-1] == "+"){ 
-            calcs.push(parseNum());
-            calcs.push(add(calcs[calcs.length-3], calcs[calcs.length-1]));
-         }
-         if(calcs[calcs.length-1] == "-"){ 
-            calcs.push(parseNum());
-            calcs.push(subtract(calcs[calcs.length-3], calcs[calcs.length-1]));
-         }
-         if(calcs[calcs.length-1] == "/"){ 
-            calcs.push(parseNum());
-            calcs.push(divide(calcs[calcs.length-3], calcs[calcs.length-1]));
-         }
-         if(calcs[calcs.length-1] == "x"){ 
-            calcs.push(parseNum());
-            calcs.push(multiply(calcs[calcs.length-3], calcs[calcs.length-1]));
-         }
-         if(calcs[calcs.length-1] == "%"){ 
-            calcs.push(parseNum());
-            calcs.push(modulus(calcs[calcs.length-3], calcs[calcs.length-1]));
-         }
-         display2.textContent = String(calcs[calcs.length-1]);
-         display1.textContent = `${calcs[calcs.length-1]} `;
-         console.log(calcs);
-         return
+        if(lastClicked.contains("operator")){
+            return;
+        }
+        else{
+            if(calcs[calcs.length-1] == "+"){ 
+                calcs.push(parseNum());
+                calcs.push(add(calcs[calcs.length-3], calcs[calcs.length-1]));
+            }
+            if(calcs[calcs.length-1] == "-"){ 
+                calcs.push(parseNum());
+                calcs.push(subtract(calcs[calcs.length-3], calcs[calcs.length-1]));
+            }
+            if(calcs[calcs.length-1] == "/"){ 
+                calcs.push(parseNum());
+                calcs.push(divide(calcs[calcs.length-3], calcs[calcs.length-1]));
+            }
+            if(calcs[calcs.length-1] == "x"){ 
+                calcs.push(parseNum());
+                calcs.push(multiply(calcs[calcs.length-3], calcs[calcs.length-1]));
+            }
+            if(calcs[calcs.length-1] == "%"){ 
+                calcs.push(parseNum());
+                calcs.push(modulus(calcs[calcs.length-3], calcs[calcs.length-1]));
+            }
+            display2.textContent = String(calcs[calcs.length-1]);
+            display1.textContent = `${calcs[calcs.length-1]} `;
+            console.log(calcs);
+            return
+        }
     }
 }
 
